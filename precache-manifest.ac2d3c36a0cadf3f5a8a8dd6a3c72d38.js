26d1508fedc3c00337e5d9246dd9517b62d39c2f
Maksym Maiboroda <crashdey323@gmail.com>
2020-05-25 23:46:00 +0300

    Updates

---
+++ precache-manifest.ac2d3c36a0cadf3f5a8a8dd6a3c72d38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fea9face5fdca8748bc240a5ec9edb",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "f09ae7b831ce21003be6",
+    "url": "/test-task/static/css/main.1a1cdee3.chunk.css"
+  },
+  {
+    "revision": "9cd71719fc2333f2c7ac",
+    "url": "/test-task/static/js/2.b771e4ef.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/test-task/static/js/2.b771e4ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09ae7b831ce21003be6",
+    "url": "/test-task/static/js/main.8dd3a2a6.chunk.js"
+  },
+  {
+    "revision": "8f71c07f9182e2aae0f0",
+    "url": "/test-task/static/js/runtime-main.9750f0fd.js"
+  },
+  {
+    "revision": "71773f6e553cd414b6486cdde5abdb88",
+    "url": "/test-task/static/media/jokeIconStatic.71773f6e.svg"
+  }
+]);
